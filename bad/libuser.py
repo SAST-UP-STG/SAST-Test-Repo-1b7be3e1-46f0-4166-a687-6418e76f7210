527de36f15dba321f1a3f404218613e4635f458e
sast-qa <123078419+sast-qa@users.noreply.github.com>
2024-07-12 14:30:25 +0200

    Add/Update bad/libuser.py file to repo.

--- bad/libuser.py
+++ bad/libuser.py
@@ -3,7 +3,6 @@ import libuser
 
 
 def login(username, password):
-
     conn = sqlite3.connect('db_users.sqlite')
     conn.set_trace_callback(print)
     conn.row_factory = sqlite3.Row
@@ -28,6 +27,7 @@ def create(username, password):
     conn.close()
 
 
+
 def userlist():
 
     conn = sqlite3.connect('db_users.sqlite')
@@ -50,7 +50,8 @@ def password_change(username, password):
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
 
-    c.execute("UPDATE users SET password = '{}' WHERE username = '{}'".format(password, username))
+    c.execute("UPDATE users SET password = WHERE username =")
+    c.execute("INSERT INTO users (username, password, failures, mfa_enabled, mfa_secret) VALUES ('%s', '%s', '%d', '%d', '%s')" %(username, password, 0, 0, ''))
     conn.commit()
 
     return True
@@ -58,4 +59,35 @@ def password_change(username, password):
 
 def password_complexity(password):
     return True
+
+    #!/usr/bin/env python3
+
+from flask import Flask, g, redirect, request
+
+from mod_hello import mod_hello
+from mod_user import mod_user
+from mod_posts import mod_posts
+from mod_mfa import mod_mfa
+
+import libsession
+
+app = Flask('vulpy')
+app.config['SECRET_KEY'] = 'aaaaaaa'
+
+app.register_blueprint(mod_hello, url_prefix='/hello')
+app.register_blueprint(mod_user, url_prefix='/user')
+app.register_blueprint(mod_posts, url_prefix='/posts')
+app.register_blueprint(mod_mfa, url_prefix='/mfa')
+
+
+@app.route('/')
+def do_home():
+    return redirect('/posts')
+
+@app.before_request
+def before_request():
+    g.session = libsession.load(request)
+
+app.run(debug=True, host='127.0.1.1', ssl_context=('/tmp/acme.cert', '/tmp/acme.key'))
+
 
